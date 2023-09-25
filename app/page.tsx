4ab01d31e9ee0d6cd17aeb6e75e24fdc466f1ba5
kiongtaro <kiongtaro@gmail.com>
2023-09-25 11:57:50 +0900

    スタイルを調整

--- app/page.tsx
+++ app/page.tsx
@@ -10,18 +10,30 @@ export default async function Home() {
     take: 3,
   });
   return (
-    <main className="container flex flex-col items-center p-8 h-screen w-full space-y-8">
-      <h1 className="text-center text-5xl font-bold text-foreground">
-        AutoFormation
+    <main className="container p-4 max-w-lg">
+      <h1 className="my-8 text-center text-8xl font-bold  text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        AutoForm
       </h1>
-      <ObjectiveForm />
-
-      {forms.map((form) => (
-        <div key={form.id}>
-          <Link href={`/${form.id}`}>{form.label}</Link>
-          <p>{form.description}</p>
-        </div>
-      ))}
+      <div className="mt-16 mb-40">
+        <ObjectiveForm />
+      </div>
+      <div className="space-y-4 mt-32">
+        <h2 className="text-2xl font-bold text-foreground text-center">
+          Recent forms
+        </h2>
+        <ul className="">
+          {forms.map((form) => (
+            <li key={form.id}>
+              <Link href={`/${form.id}`}>
+                <h3 className="text-xl font-bold text-foreground/80">
+                  {form.label}
+                </h3>
+              </Link>
+              <p className="text-base text-foreground/50">{form.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
