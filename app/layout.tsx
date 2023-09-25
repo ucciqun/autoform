8bbcad8af6f8d081eee7fffe80541570441c7110
ucciqun <zerda.elsword@gmail.com>
2023-09-25 15:38:58 +0900

    軽微な修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <header className="p-4">
-          <Link href="/" className="flex items-center gap-2">
+          <Link href="/" className=" items-center gap-2 inline-flex">
             <Disc3 className="w-6 h-6 animate-spin" />
             <h1 className="text-xl font-bold text-foreground italic">
               AutoForm
