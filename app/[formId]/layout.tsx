dc182870ae4aff96c5f323d2efada5ceba82b257
ucciqun <zerda.elsword@gmail.com>
2023-09-25 17:03:05 +0900

    戻るボタンを追加

--- app/[formId]/layout.tsx
+++ app/[formId]/layout.tsx
@@ -2,9 +2,9 @@
 
 import { CopyButton } from "@/components/copy-button";
 import { Button } from "@/components/ui/button";
-import { ExternalLink, Link as LinkIcon } from "lucide-react";
+import { ArrowLeft, ExternalLink, Link as LinkIcon } from "lucide-react";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -14,22 +14,33 @@ interface LayoutProps {
 export default function Layout({ children, params }: LayoutProps) {
   const searchParams = useSearchParams();
   const isEdit = searchParams.get("mode") === "edit";
+  const router = useRouter();
   return (
     <div className="">
       {isEdit && (
-        <header className="flex justify-end p-4 border-b border-foreground/10 gap-4">
-          <Button variant="outline" size="sm" asChild>
-            <Link href={`/${params.formId}`} rel="noopener noreferrer">
-              <ExternalLink className="w-4 h-4 mr-2" />
-              Goto Preview
-            </Link>
+        <header className="flex justify-between p-4 border-b border-foreground/10 gap-4">
+          <Button variant="ghost" size="sm" onClick={() => router.back()}>
+            <ArrowLeft className="w-4 h-4 mr-2" />
+            Back
           </Button>
-          <CopyButton
-            url={`http://localhost:3000/${params.formId}`}
-            icon={<LinkIcon className="w-4 h-4 mr-2" />}
-          >
-            Share
-          </CopyButton>
+          <div className="flex items-center gap-4">
+            <Button variant="outline" size="sm" asChild>
+              <Link
+                href={`/${params.formId}`}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <ExternalLink className="w-4 h-4 mr-2" />
+                Open Form
+              </Link>
+            </Button>
+            <CopyButton
+              url={`http://localhost:3000/${params.formId}`}
+              icon={<LinkIcon className="w-4 h-4 mr-2" />}
+            >
+              Share
+            </CopyButton>
+          </div>
         </header>
       )}
       <div className={`${isEdit && "bg-foreground/5"}`}>{children}</div>
