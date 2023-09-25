52af59aa9ee73ed3788c2f7a3e628969d5e33f27
kiongtaro <kiongtaro@gmail.com>
2023-09-25 11:58:03 +0900

    規定値を追加

--- components/objective-form.tsx
+++ components/objective-form.tsx
@@ -12,16 +12,22 @@ import {
   FormLabel,
   FormMessage,
 } from "./ui/form";
-import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
-import { Loader2 } from "lucide-react";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { Disc3 } from "lucide-react";
+import { Input } from "./ui/input";
 
 export function ObjectiveForm() {
   const [loading, setLoading] = useState(false);
   const form = useForm<z.infer<typeof objectiveFormSchema>>({
     resolver: zodResolver(objectiveFormSchema),
+    defaultValues: {
+      objective: {
+        type: "textarea",
+        value: "",
+      },
+    },
   });
   const router = useRouter();
 
@@ -38,25 +44,37 @@ export function ObjectiveForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex items-center space-x-2 drop-shadow-xl"
+      >
         <FormField
           control={form.control}
           name="objective.value"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>
+            <FormItem className="flex-1 space-y-0">
+              <FormLabel className="sr-only">
                 What goals do you hope to achieve with your survey?
               </FormLabel>
               <FormControl>
-                <Textarea {...field} placeholder="Type your answer here." />
+                <Input
+                  placeholder="What goals do you hope to achieve with your survey?"
+                  className="bg-foreground font-semibold duration-150 text-white rounded-full p-5"
+                  {...field}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit" disabled={loading} className="w-full">
-          {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-          Create a new form
+        <Button
+          type="submit"
+          disabled={loading}
+          className="bg-foreground rounded-full"
+          size="icon"
+        >
+          <Disc3 className={`h-4 w-4  ${loading && "animate-spin"}`} />
+          {/* Generate a new form */}
         </Button>
       </form>
     </Form>
