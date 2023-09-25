5bffc8e92af0900d15a6cd8638628ec3aead6162
ucciqun <zerda.elsword@gmail.com>
2023-09-25 17:02:55 +0900

    フォーム一覧をカードに

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import { ObjectiveForm } from "@/components/objective-form";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { db } from "@/lib/db";
+import { MoveRight, Settings2 } from "lucide-react";
 import Link from "next/link";
 
 export default async function Home() {
@@ -18,18 +27,39 @@ export default async function Home() {
         <ObjectiveForm />
       </div>
       <div className="space-y-4 mt-32">
-        <h2 className="text-2xl font-bold text-foreground text-center">
+        <h2 className="text-2xl font-bold text-foreground/80 flex gap-4 items-center px-2">
+          <span className="relative flex h-3 w-3">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+          </span>
           Recent forms
         </h2>
-        <ul className="">
+        <ul className="space-y-4">
           {forms.map((form) => (
             <li key={form.id}>
-              <Link href={`/${form.id}`}>
-                <h3 className="text-xl font-bold text-foreground/80">
-                  {form.label}
-                </h3>
-              </Link>
-              <p className="text-base text-foreground/50">{form.description}</p>
+              <Card>
+                <CardHeader>
+                  <CardTitle className="flex justify-between items-center">
+                    <Link href={`/${form.id}`} className="hover:underline">
+                      {form.label}
+                    </Link>
+                    <div>
+                      {/* <Button asChild variant="outline" size="sm">
+                        <Link href={`/${form.id}`}>
+                          Open
+                          <MoveRight className="w-4 h-4 ml-2" />
+                        </Link>
+                      </Button> */}
+                      <Button variant="ghost" size="icon" asChild>
+                        <Link href={`/${form.id}?mode=edit`}>
+                          <Settings2 className="w-4 h-4" />
+                        </Link>
+                      </Button>
+                    </div>
+                  </CardTitle>
+                  <CardDescription>{form.description}</CardDescription>
+                </CardHeader>
+              </Card>
             </li>
           ))}
         </ul>
